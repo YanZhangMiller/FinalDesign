7212b60dc7c850dc354442e0521fdfde5b17eda9
YanZhangMiller <yanzhangmiller@gmail.com>
2014-04-14 17:26:03 +0800

    Unfinished change

--- src/org/path/buaa/PathProcessor.java
+++ src/org/path/buaa/PathProcessor.java
@@ -2,13 +2,35 @@ package org.path.buaa;
 
 import org.tools.buaa.MySql;
 
-public class PathProcessor {
+public class PathProcessor implements Runnable{
 
 	MySql m = new MySql();
-	
+	volatile boolean isStop = false;
 	public PathProcessor()
 	{
 		m.setCover(true);
+	}
+	/**
+	 * stop calculation of News Path.
+	 */
+	public void stop()
+	{
+		isStop = true;
+	}
+	public void run()
+	{
+		while (!isStop)
+		{
+			
+			
+			try {
+				Thread.sleep(300000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+		}
+		
 	}
 	
 	
