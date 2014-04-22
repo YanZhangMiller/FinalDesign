3e8863a03259f411ce6acfe0bea795bbe5d2d430
YanZhangMiller <yanzhangmiller@gmail.com>
2014-04-22 20:03:45 +0800

    database

--- webpage/news/newspath/views.py
+++ webpage/news/newspath/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def search_news(request):
+    from django.db import connection, transaction
+    cursor = connection.cursor()
+    cursor.excute("SHOW TABLES")
+    
+
